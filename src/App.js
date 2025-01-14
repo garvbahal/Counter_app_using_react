44011b124982729b43f9fe5aa76d45b9a845782b
garvbahal <garvbahal75@gmail.com>
2025-01-15 00:00:42 +0530

    final-commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,51 @@
 import "./App.css";
+import { useState } from "react";
 
 function App() {
-  return <div className="App"></div>;
+  const [count, setCount] = useState(0);
+
+  function minusHandler() {
+    setCount(count - 1);
+  }
+  function plusHandler() {
+    setCount(count + 1);
+  }
+
+  function resetHandler() {
+    setCount(0);
+  }
+
+  return (
+    // wrapper
+    <div className="w-[100vw] h-[100vh] flex justify-center items-center bg-[#344151] flex-col gap-10">
+      <div className="text-[#0398d4] font-medium text-2xl">
+        Increment & Decrement
+      </div>
+
+      <div className="flex bg-white items-center gap-12 py-3 rounded-sm justify-center text-[25px] text-[#344151]">
+        <button
+          onClick={minusHandler}
+          className="border-r-2 text-center w-20 border-[#bfbfbf] text-5xl"
+        >
+          -
+        </button>
+        <div className="font-bold gap-12 text-5xl">{count}</div>
+        <button
+          onClick={plusHandler}
+          className="border-l-2 text-center w-20 border-[#bfbfbf] text-5xl"
+        >
+          +
+        </button>
+      </div>
+
+      <button
+        onClick={resetHandler}
+        className="bg-[#0398d4] text-white px-5 py-2 rounded-sm text-lg"
+      >
+        Reset
+      </button>
+    </div>
+  );
 }
 
 export default App;
